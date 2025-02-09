c387fca6a2aec92aa80691ecbd6c9f1ce619eb29
Mahmoud Morsy <morsy1238@gmail.com>
2025-02-09 15:53:53 +0200

    add animation to close alarm

--- app/src/main/java/com/example/tiaalert/AlarmDetailsActivity.kt
+++ app/src/main/java/com/example/tiaalert/AlarmDetailsActivity.kt
@@ -1,9 +1,9 @@
 package com.example.tiaalert
 
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
 import com.example.tiaalert.main.AlarmRing
 import com.example.tiaalert.main.model.AlarmsModel
 import com.example.tiaalert.main.utils.fromJson
@@ -15,7 +15,7 @@ class AlarmDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_details)
 
-//        val motionLayout: MotionLayout = findViewById(R.id.motionLayout)
+        val motionLayout: MotionLayout = findViewById(R.id.motionLayout)
 
 
         val alarmJson = intent.getStringExtra("alarm")
@@ -25,43 +25,43 @@ class AlarmDetailsActivity : AppCompatActivity() {
             alarm?.name ?: getString(R.string.without_name)
 
 
-//        // زر إيقاف النغمة
-//        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
-//            override fun onTransitionStarted(
-//                motionLayout: MotionLayout?,
-//                startId: Int,
-//                endId: Int
-//            ) {
-//            }
-//
-//            override fun onTransitionChange(
-//                motionLayout: MotionLayout?,
-//                startId: Int,
-//                endId: Int,
-//                progress: Float
-//            ) {
-//            }
-//
-//            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-//                if (currentId == R.id.end) {
-//                    AlarmRing.stop()
-//                    finish() // إغلاق الشاشة بعد السحب
-//                }
-//            }
-//
-//            override fun onTransitionTrigger(
-//                motionLayout: MotionLayout?,
-//                triggerId: Int,
-//                positive: Boolean,
-//                progress: Float
-//            ) {
-//            }
-//        })
+        // زر إيقاف النغمة
+        motionLayout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int
+            ) {
+            }
 
-        findViewById<Button>(R.id.stopAlarmButton).setOnClickListener {
-            AlarmRing.stop()
-            finish() // إغلاق الشاشة بعد السحب
-        }
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                if (currentId == R.id.end) {
+                    AlarmRing.stop()
+                    finish() // إغلاق الشاشة بعد السحب
+                }
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+            }
+        })
+
+//        findViewById<Button>(R.id.stopAlarmButton).setOnClickListener {
+//            AlarmRing.stop()
+//            finish() // إغلاق الشاشة بعد السحب
+//        }
 
     }
 
